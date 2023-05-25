c163205f46a1d4eb875c515d61e57ffee095fef2
markgaal068 <gaalmark.marki@gmail.com>
2023-05-25 16:15:01 +0200

    /email

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ ScrollReveal().reveal('.home-img, .teams-container, .news-container, .contact fo
             "tel": document.getElementById('phone').value
         };
 
-        fetch("https://rich-ruby-indri-hat.cyclic.app", {
+        fetch("https://rich-ruby-indri-hat.cyclic.app/email", {
             method: "POST", // or 'PUT'
             headers: {
                 "Content-Type": "application/json",
